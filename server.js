6090db2404654222650ddca464fe813bc3e24cb0
oleg0031 <oleg0031@gmail.com>
2022-04-26 15:25:12 +0300

    update

--- server.js
+++ server.js
@@ -3,14 +3,6 @@ const stripe = require('stripe')(process.env.REACT_APP_TEST_KEY);
 const express = require('express')
 const app = express()
 
-
-const path = require("path")
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"))
-  app.get("*", (req, res) => {
-    req.sendFile(path.resolve(__dirname, "build", "index.html"))
-  })
-}
 app.post('/create-checkout-session', async (req, res) => {
 
   const session = await stripe.checkout.sessions.create({
